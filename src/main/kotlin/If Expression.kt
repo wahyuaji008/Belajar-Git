d752c985e70c1d3f5a1d1e7ae53a50dd5a25ec71
wahyuaji008 <minyakgoreng358@gmail.com>
2020-09-20 11:45:57 +0700

    KotlinFundamental

---
+++ src/main/kotlin/If Expression.kt
@@ -0,0 +1,13 @@
+fun main(){
+    val kkm = 75
+    val hasil = 80
+    val nilai : String
+    nilai = if (hasil >= kkm){
+        "Nilai kamu $hasil, selamat ya"
+    } else {
+        "nilai kamu $hasil, silakan remidial"
+    }
+    print (nilai)
+
+
+}
